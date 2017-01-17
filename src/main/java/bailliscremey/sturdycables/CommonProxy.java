6a7743ce4e4ef9528a09c9712001798d46da8d22
BaillisCremey <bailiscremey@gmail.com>
2017-01-17 12:05:06 -0800

    Update CommonProxy.java

--- src/main/java/bailliscremey/sturdycables/CommonProxy.java
+++ src/main/java/bailliscremey/sturdycables/CommonProxy.java
@@ -1,1 +1,21 @@
+package bailliscremey.sturdycables;
 //common proxy for Cables Mod
+
+public class CommonProxy
+{
+
+    public void preInit(FMLPreInitializationEvent e)
+    {
+
+    }
+
+    public void init(FMLInitializationEvent e)
+    {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent e)
+    {
+
+    }
+}
