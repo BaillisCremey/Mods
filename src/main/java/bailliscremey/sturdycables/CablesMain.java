cb27acfdd3a4317a4c199df12a72b87dcdb136ec
BaillisCremey <bailiscremey@gmail.com>
2017-01-17 12:23:55 -0800

    Update CablesMain.java

--- src/main/java/bailliscremey/sturdycables/CablesMain.java
+++ src/main/java/bailliscremey/sturdycables/CablesMain.java
@@ -7,10 +7,13 @@ public class Main
 public static String MODID = "SturdyCables";
 public static String VERSION = "0.0.0";
 
+@SidedProxy(clientSide="bailliscremey.sturdycables.ClientProxy", serverSide="bailliscremey.sturdycables.ServerProxy")
+public static CommonProxy proxy;
+
 @EventHandler
 public void preInit(FMLPreInitializationEvent e)
 {
-  co
+    
 }
 
 @EventHandler
