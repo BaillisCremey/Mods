62961cf3b2241d083ec30b724960d4cc8074b412
BaillisCremey <bailiscremey@gmail.com>
2017-01-17 11:56:43 -0800

    Update CablesMain.java

--- src/main/java/bailliscremey/CablesMain.java
+++ src/main/java/bailliscremey/CablesMain.java
@@ -1,1 +1,25 @@
+package bailliscremey.cables;
 //main class for Cables Mod
+
+@Mod(modid="SturdyCables", version="0.0.0")
+public class Main
+{
+public static String MODID = "SturdyCables";
+public static String VERSION = "0.0.0";
+
+@EventHandler
+public void preInit(FMLPreInitializationEvent e)
+{
+  co
+}
+
+@EventHandler
+public void init(FMLInitializationEvent e)
+{
+}
+
+@EventHandler
+public void postInit(FMLPostInitializationEvent e)
+{
+}
+}
