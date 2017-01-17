5aea99b5163f64e1139f2b38fe77fafc32204d47
BaillisCremey <bailiscremey@gmail.com>
2017-01-17 12:28:35 -0800

    Update CablesMain.java

--- src/main/java/bailliscremey/sturdycables/CablesMain.java
+++ src/main/java/bailliscremey/sturdycables/CablesMain.java
@@ -4,25 +4,27 @@ package bailliscremey.sturdycables;
 @Mod(modid="SturdyCables", version="0.0.0")
 public class Main
 {
-public static String MODID = "SturdyCables";
-public static String VERSION = "0.0.0";
+    public static String MODID = "SturdyCables";
+    public static String VERSION = "0.0.0";
 
-@SidedProxy(clientSide="bailliscremey.sturdycables.ClientProxy", serverSide="bailliscremey.sturdycables.ServerProxy")
-public static CommonProxy proxy;
+    @SidedProxy(clientSide = "bailliscremey.sturdycables.ClientProxy", serverSide = "bailliscremey.sturdycables.ServerProxy")
+    public static CommonProxy proxy;
 
-@EventHandler
-public void preInit(FMLPreInitializationEvent e)
-{
-    
-}
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e)
+    {
+        proxy.preInit(e);
+    }
 
-@EventHandler
-public void init(FMLInitializationEvent e)
-{
-}
+    @EventHandler
+    public void init(FMLInitializationEvent e)
+    {
+        proxy.init(e);
+    }
 
-@EventHandler
-public void postInit(FMLPostInitializationEvent e)
-{
-}
+    @EventHandler
+    public void     postInit(FMLPostInitializationEvent e)
+    {
+
+    }
 }
