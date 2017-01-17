311d5ca51977c2785c5191a3fa585b382fd144f9
BaillisCremey <bailiscremey@gmail.com>
2017-01-17 12:19:52 -0800

    Update ClientProxy.java

--- src/main/java/bailliscremey/sturdycables/ClientProxy.java
+++ src/main/java/bailliscremey/sturdycables/ClientProxy.java
@@ -1,1 +1,25 @@
+package bailliscremey.sturdycables;
 //client proxy for Cables Mod
+
+public class ClientProxy extends CommonProxy
+{
+
+    @Override
+    public void preInit(FMLPreInitializationEvent e)
+    {
+        super.preInit(e);
+    }
+
+    @Override
+    public void init(FMLInitializationEvent e)
+    {
+        super.init(e);
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent e)
+    {
+        super.postInit(e);
+    }
+
+}
